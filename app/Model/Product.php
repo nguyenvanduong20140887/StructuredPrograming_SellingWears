4bc5fd433e4f83db9ba7637f320740d12b54a20e
01101100 <long.dragon96@gmail.com>
2017-11-01 21:51:24 +0700

    Define relationships between models

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $primaryKey = 'prod_id';
+
+    public function category()
+    {
+    	return $this->belongsTo('App\Model\Category', 'category', 'category');
+    }
+
+    public function orderlines()
+    {
+    	return $this->hasMany('App\Model\Orderline', 'prod_id', 'prod_id');
+    }
 }
